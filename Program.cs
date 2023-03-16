8165b82ea3c2c9bdbe13c69a4e2ba467e22a3818
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-03-15 20:45:20 -0700

    if statements and conversions

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Excersize_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if statements
+
+            Console.WriteLine();
+            int age = int.Parse(Console.ReadLine());
+
+            if(age >= 18)
+            {
+                Console.WriteLine("You are old enough to enter.");
+            }
+
+
+           
+            Console.WriteLine(Convert.ToString(age));    // convert int to string
+
+            Console.WriteLine(Convert.ToDouble(age));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(age));  // convert double to int
+            Console.WriteLine(Convert.ToString(age));   // convert bool to string
+            
+        }
+    }
+}
